31165fcb166281efa963fd7bf567aae0d38fff2e
mvahedi <vahedi.ce@gmail.com>
2021-11-10 15:16:53 +0330

    feat: add parcel and remove rollup

--- src/useClipboard/useClipboard.tsx
+++ src/useClipboard/useClipboard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useClipboard = (text: string) => {
   const [isCopied, setIsCopied] = useState<boolean>(false);
